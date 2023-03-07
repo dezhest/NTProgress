4d90676025c64d98892057834e4c68a20ecc7b7d
dezhest <dezhest@yandex.ru>
2023-03-07 19:42:38 +0400

    Initial Commit

---
+++ NTProgressTest/NTProgressTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTProgressTestApp.swift
+//  NTProgressTest
+//
+//  Created by Denis Zhesterev on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NTProgressTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
