6be6ac77fcd1bd264b3b39cb6d019eebc334e850
dezhest <dezhest@yandex.ru>
2023-03-16 22:10:44 +0400

    3/5 sort

--- NTProgressTest/DealsViewModel.swift
+++ NTProgressTest/DealsViewModel.swift
@@ -10,18 +10,17 @@ import SwiftUI
 class DealsViewModel: ObservableObject {
     private let server = Server()
     @Published var model = DealsModel()
-    @Published var deals: [Deal] = []
+    @Published var deals: [Deal] = [] 
+    var dealsDateUp: [Deal] = []
+    var dealsPriceUp: [Deal] = []
+    var dealsAmountUp: [Deal] = []
+    var dealsSideUp: [Deal] = []
+    var dealsSideDown: [Deal] = []
+    var dealsName: [Deal] = []
     private var newDealsToSort: [Deal] = []
     private var timer = Timer()
     
     lazy var sortingMethod = dataSortUp
-//    {
-//        willSet {
-//            DispatchQueue.main.async { [self] in
-//                deals = deals.sorted(by: newValue)
-//            }
-//        }
-//    }
     
     var dataSortUp: (Deal, Deal) -> Bool = { (deal: Deal, deal2: Deal) -> Bool in
         deal.dateModifier.timeIntervalSince1970 < deal2.dateModifier.timeIntervalSince1970
@@ -57,10 +56,31 @@ class DealsViewModel: ObservableObject {
     
     func updateDeals() {
         print("UPDATE \(newDealsToSort.count) \(deals.count)")
-        
-            deals = getMergedDataArray(sortedData: deals, newData: newDealsToSort)
+        switch (model.selectedSortingOption, model.destinationArrow) {
+        case(.date, true): deals = dealsDateUp
+        case(.price, true): deals = dealsPriceUp
+        case(.amount, true): deals = dealsAmountUp
+        case(.side, true): deals = dealsSideUp
+        case(.date, false): deals = dealsDateUp.reversed()
+        case(.price, false): deals = dealsPriceUp.reversed()
+        case(.amount, false): deals = dealsAmountUp.reversed()
+        case(.side, false): deals = dealsSideDown
+        default: print("default")
+        }
+        DispatchQueue.main.async { [self] in
+            dealsDateUp = getMergedDataArrayDate(sortedData: dealsDateUp, newData: newDealsToSort)
+            
+            dealsPriceUp = getMergedDataArrayPrice(sortedData: dealsPriceUp, newData: newDealsToSort)
+            
+            dealsAmountUp = getMergedDataArrayAmount(sortedData: dealsAmountUp, newData: newDealsToSort)
+//            
+//                    dealsSideUp = getMergedDataArraySideUp(sortedData: deals, newData: newDealsToSort)
+//            dealsSideDown = getMergedDataArraySideDown(sortedData: deals, newData: newDealsToSort)
+            //        dealsName = getMergedDataArrayName(sortedData: deals, newData: newDealsToSort)
+            
             newDealsToSort = []
-        
+        }
+      
     }
     
     func startDealsPipe() {
@@ -70,23 +90,21 @@ class DealsViewModel: ObservableObject {
         self.server.subscribeToDeals { [self] newDeals in
             DispatchQueue.main.async { [self] in
                 if deals.isEmpty {
-                    deals = getMergedDataArray(sortedData: [], newData: newDeals)
+                    dealsDateUp = getMergedDataArrayDate(sortedData:  [], newData: newDeals)
                 } else {
                     newDealsToSort += newDeals
                 }
             }
         }
     }
-    
-    private func getMergedDataArray(sortedData: [Deal], newData: [Deal]) -> [Deal] {
-      let sortedNewData = newData.sorted(by: sortingMethod)
+    private func getMergedDataArrayDate(sortedData: [Deal], newData: [Deal]) -> [Deal] {
+        let sortedNewData = newData.sorted(by: {$0.dateModifier > $1.dateModifier})
       var mergedData = [Deal]()
       mergedData.reserveCapacity(sortedData.count + newData.count)
       var i = 0
       var j = 0
       while i < sortedData.count && j < sortedNewData.count {
-        if sortingMethod(sortedData[i], sortedNewData[j]) {
-//          if sortedData[i].price < sortedNewData[j].price {
+          if sortedData[i].dateModifier > sortedNewData[j].dateModifier {
           mergedData.append(sortedData[i])
           i += 1
         } else {
@@ -104,4 +122,104 @@ class DealsViewModel: ObservableObject {
       }
       return mergedData
     }
+    private func getMergedDataArrayAmount(sortedData: [Deal], newData: [Deal]) -> [Deal] {
+        let sortedNewData = newData.sorted(by: {$0.amount > $1.amount})
+      var mergedData = [Deal]()
+      mergedData.reserveCapacity(sortedData.count + newData.count)
+      var i = 0
+      var j = 0
+      while i < sortedData.count && j < sortedNewData.count {
+          if sortedData[i].amount > sortedNewData[j].amount {
+          mergedData.append(sortedData[i])
+          i += 1
+        } else {
+          mergedData.append(sortedNewData[j])
+          j += 1
+        }
+      }
+      while i < sortedData.count {
+        mergedData.append(sortedData[i])
+        i += 1
+      }
+      while j < sortedNewData.count {
+        mergedData.append(sortedNewData[j])
+        j += 1
+      }
+      return mergedData
+    }
+    private func getMergedDataArrayPrice(sortedData: [Deal], newData: [Deal]) -> [Deal] {
+        let sortedNewData = newData.sorted(by: {$0.price > $1.price})
+      var mergedData = [Deal]()
+      mergedData.reserveCapacity(sortedData.count + newData.count)
+      var i = 0
+      var j = 0
+      while i < sortedData.count && j < sortedNewData.count {
+          if sortedData[i].price > sortedNewData[j].price {
+          mergedData.append(sortedData[i])
+          i += 1
+        } else {
+          mergedData.append(sortedNewData[j])
+          j += 1
+        }
+      }
+      while i < sortedData.count {
+        mergedData.append(sortedData[i])
+        i += 1
+      }
+      while j < sortedNewData.count {
+        mergedData.append(sortedNewData[j])
+        j += 1
+      }
+      return mergedData
+    }
+
+    func getMergedDataArraySideUp(sortedData: [Deal], newData: [Deal]) -> [Deal] {
+        let sortedFirstArray: [Deal] = sortedData
+        let sortedSecondArray = newData.sorted(by: sideSortUp)
+        var mergedArray = [Deal]()
+        var firstIndex = 0
+        var secondIndex = 0
+        while firstIndex < sortedFirstArray.count && secondIndex < sortedSecondArray.count {
+            if sortedFirstArray[firstIndex].side == .buy {
+                mergedArray.append(sortedFirstArray[firstIndex])
+            }
+            firstIndex += 1
+            if sortedSecondArray[secondIndex].side == .buy {
+                mergedArray.append(sortedSecondArray[secondIndex])
+            }
+            secondIndex += 1
+        }
+        if firstIndex < sortedFirstArray.count {
+            mergedArray.append(contentsOf: sortedFirstArray[firstIndex...])
+        }
+        if secondIndex < sortedSecondArray.count {
+            mergedArray.append(contentsOf: sortedSecondArray[secondIndex...])
+        }
+        return mergedArray
+    }
+    func getMergedDataArraySideDown(sortedData: [Deal], newData: [Deal]) -> [Deal] {
+        let sortedFirstArray: [Deal] = sortedData
+        let sortedSecondArray = newData.sorted(by: sideSortDown)
+        var mergedArray = [Deal]()
+        var firstIndex = 0
+        var secondIndex = 0
+        while firstIndex < sortedFirstArray.count && secondIndex < sortedSecondArray.count {
+            if sortedFirstArray[firstIndex].side == .sell {
+                mergedArray.append(sortedFirstArray[firstIndex])
+            }
+            firstIndex += 1
+            if sortedSecondArray[secondIndex].side == .sell {
+                mergedArray.append(sortedSecondArray[secondIndex])
+            }
+            secondIndex += 1
+        }
+        if firstIndex < sortedFirstArray.count {
+            mergedArray.append(contentsOf: sortedFirstArray[firstIndex...])
+        }
+        if secondIndex < sortedSecondArray.count {
+            mergedArray.append(contentsOf: sortedSecondArray[secondIndex...])
+        }
+        return mergedArray
+    }
+
 }
